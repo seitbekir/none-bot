668c3cb12e460138d37f7853bae111956528c5e6
seitbekir <wided@ya.ru>
2017-09-04 13:29:09 +0300

    More stability

--- index.js
+++ index.js
@@ -1,46 +1,76 @@
 const VK = require('node-vk-bot-api')
+var fs = require('fs')
 
 const bot = new VK({ token: process.env.TOKEN })
 
-const hyash = [
-    'Ты прекрасна!',
-    'Твое лицо как фиолент!',
-    'Трахнул бы тебя с включенным светом!',
-    'Я люблю тебя!',
-    'Я не пидор',
-    'У тебя есть три.. нет, бесконечно желаний,к оторые я хотел бы исполнить для тебя!',
-    'Хочешь я спою тебе песню?\nКонечно хочешь. Но я пока не могу ((',
-    'Извини, я туповат',
-]
-const lovers = [];
+const niceWordsSource = process.env.NICE_WORDS
+const clientsSource = process.env.CLIENTS
+const adminId = process.env.ADMIN
 
-bot.hears(['Привет', 'привет', 'Привет!', 'привет!', 'прив', 'ghbdtn'], (ctx) => {
-  ctx.sendMessage(ctx.user_id, 'Привет, няш!')
+const commands = {
+    hello: ['привет','привет!', 'прив', 'ghbdtn'],
+    say: ['говори', 'ujdjhb'],
+    love: ['люби'],
+    stop: ['перестань'],
+    fooo: ['хуйня', 'заебал'],
+
+    // admin
+    admin: {
+        niceSet: '/nice-set',
+        niceUnset: '/nice-unset',
+    }
+}
+
+bot.hears(commands.hello, (ctx) => {
+    ctx.sendMessage(ctx.user_id, 'Привет, няш!')
 })
 
-bot.hears(['говори', 'Говори', 'ujdjhb'], (ctx) => {
-  ctx.sendMessage(ctx.user_id, hyash[randomInteger(0, hyash.length - 1)])
+bot.hears(commands.say, (ctx) => {
+    ctx.sendMessage(ctx.user_id, getNiceWord())
 })
 
-bot.hears('люби', (ctx) => {
-    if (lovers.indexOf(ctx.user_id) === -1) {
+bot.hears(commands.love, (ctx) => {
+    if (setClient(ctx.user_id)) {
         ctx.sendMessage(ctx.user_id, "Каждый час ты будешь получать фразу ❤☺")
-        lovers.push(ctx.user_id);
     } else {
         ctx.sendMessage(ctx.user_id, "Я уже тебя люблю ❤💋")
     }
 })
-bot.hears('перестань', (ctx) => {
-    if (lovers.indexOf(ctx.user_id) > -1) {
+bot.hears(commands.stop, (ctx) => {
+    if (unsetClient(ctx.user_id)) {
         ctx.sendMessage(ctx.user_id, "Ладно, больше не будешь получать фраз 😔")
-        lovers.splice(lovers.indexOf(ctx.user_id), 1);
     } else {
         ctx.sendMessage(ctx.user_id, "Я и не начинал ☺")
     }
 })
 
+bot.hears(commands.admin.niceSet, (ctx) => {
+    if (!admin(ctx)) {
+        return
+    }
+    let word = ctx.body.slice(commands.admin.niceSet.length).trim().split('\n').join('\\n')
+
+    if (setNiceWord(word)) {
+        ctx.reply('Ok')
+    } else {
+        ctx.reply('Exists:\n' + word)
+    }
+})
+bot.hears(commands.admin.niceUnset, (ctx) => {
+    if (!admin(ctx)) {
+        return
+    }
+    let word = ctx.body.slice(commands.admin.niceUnset.length).trim().split('\n').join('\\n')
+
+    if (unsetNiceWord(word)) {
+        ctx.reply('Ok')
+    } else {
+        ctx.reply('Not Exists:\n' + word)
+    }
+})
+
 bot.on((ctx) => {
-  ctx.reply('Я знаю только три команды: "привет", "говори", "люби" и "перестань"')
+    ctx.reply('Я знаю только эти команды: "привет", "говори", "люби" и "перестань"')
 })
 
 bot.listen()
@@ -49,11 +79,23 @@ bot.listen()
  * each hour it sends one message to exeryone, who subscribed.
  */
 setInterval(() => {
-    let ph = hyash[randomInteger(0, hyash.length - 1)]
-    lovers.forEach(lover => {
-        bot.reply(lover, ph)
+    let niceWord = getNiceWord()
+    let clients = getClients()
+
+    clients.forEach(client => {
+        bot.reply(client, niceWord)
     })
 }, 60 * 60 * 1000)
+// }, 30 * 1000)
+
+/* Private */
+function admin(ctx) {
+    if (ctx.user_id.toString() === adminId.toString()) {
+        return true
+    }
+    ctx.sendMessage(ctx.user_id, "Это не для вас написано ☺")
+    return false
+}
 
 /**
  * Random integer random
@@ -64,7 +106,93 @@ setInterval(() => {
  * @returns {Number} random integer number
  */
 function randomInteger(min, max) {
-    var rand = min + Math.random() * (max + 1 - min);
-    rand = Math.floor(rand);
-    return rand;
-  }
+    var rand = min + Math.random() * (max + 1 - min)
+    rand = Math.floor(rand)
+    return rand
+}
+
+function getNiceWord() {
+    let niceWords = getNiceWords()
+    return niceWords[randomInteger(0, niceWords.length - 1)]
+}
+function getNiceWords() {
+    try {
+        let niceWords = fs.readFileSync(niceWordsSource).toString().split("\n")
+        return niceWords.filter(e => e !== "")
+    } catch(err) {
+        console.error(err)
+        return []
+    }
+}
+function setNiceWord(word) {
+    try {
+        let niceWords = getNiceWords()
+        if (niceWords.indexOf(word) === -1) {
+            niceWords.push(word)
+            
+            fs.writeFileSync(niceWordsSource, niceWords.join('\n'))
+            return true
+        }
+        return false
+    } catch(err) {
+        console.error(err)
+        return false
+    }
+}
+function unsetNiceWord(word) {
+    try {
+        let niceWords = getNiceWords()
+        if (niceWords.indexOf(word) > -1) {
+            niceWords.splice(niceWords.indexOf(word), 1)
+
+            fs.writeFileSync(niceWordsSource, niceWords.join('\n'))
+            return true
+        }
+        return false
+    } catch(err) {
+        console.error(err)
+        return false
+    }
+}
+
+function getClients() {
+    try {
+        let clients = fs.readFileSync(clientsSource).toString().split("\n")
+        return clients.filter(e => e !== "")
+    } catch(err) {
+        console.error(err)
+        return []
+    }
+}
+function setClient(peerId) {
+    try {
+        peerId = peerId.toString()
+        let clients = getClients()
+        if (clients.indexOf(peerId) === -1) {
+            clients.push(peerId)
+            
+            fs.writeFileSync(clientsSource, clients.join('\n'))
+            return true
+        }
+        return false
+    } catch(err) {
+        console.error(err)
+        return false
+    }
+}
+function unsetClient(peerId) {
+    try {
+        peerId = peerId.toString()
+        let clients = getClients()
+        if (clients.indexOf(peerId) > -1) {
+            clients.splice(clients.indexOf(peerId), 1)
+
+            fs.writeFileSync(clientsSource, clients.join('\n'))
+            return true
+        }
+        return false
+    } catch(err) {
+        console.error(err)
+        return false
+    }
+}
